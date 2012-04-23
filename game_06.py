d5e1c900bc8547cf831efa986159edb7859787ab
Nathan D Karimian <ndk01u@avon.cs.ad.nottingham.ac.uk>
2012-04-23 15:59:22 +0100

    added game06

---
+++ game_06.py
@@ -0,0 +1,31 @@
+import pygame, sys
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((400,400),0,32)
+    screen = pygame.display.get_surface()
+    color = pygame.Color(20,40,80)
+    mx,my = 0,0
+    obj = pygame.image.load('sprite.png').convert_alpha()
+    screen.fill(color)
+    pygame.mouse.set_visible(0)
+    
+    while 1:
+        objR = obj.get_rect()
+        objR.center = (mx,my)
+
+        screen.fill(color)
+        screen.blit(obj,objR)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOUSEMOTION:
+                mx,my = event.pos
+        pygame.display.update()
+        
+
+    
+main()
+        
