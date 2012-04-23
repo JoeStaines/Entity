5959234b652673d7b3f989c989625806bc38f117
Joe Staines <jxs31u@avon.cs.ad.nottingham.ac.uk>
2012-04-23 18:18:04 +0100

    Added enemy class

--- game_06.py
+++ game_06.py
@@ -1,44 +1,20 @@
 import pygame, sys
 from pygame.locals import *
+    	
 
-"""
-def main():
-    pygame.init()
-    window = pygame.display.set_mode((400,400),0,32)
-    screen = pygame.display.get_surface()
-    color = pygame.Color(20,40,80)
-    mx,my = 0,0
-    obj = pygame.image.load('sprite.png').convert_alpha()
-    screen.fill(color)
-    pygame.mouse.set_visible(0)
-    
-    while 1:
-        objR = obj.get_rect()
-        objR.center = (mx,my)
-
-        screen.fill(color)
-        screen.blit(obj,objR)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == MOUSEMOTION:
-                mx,my = event.pos
-        pygame.display.update()
-    
-"""    
-
-
-def main():
-	#some shit
-	pygame.init()
-	
-
-class player(pygame.sprite.Sprite):
-	def __init__(self,setIniLoc):
-		
-
-
-
-main()
-        
+class EnemyObject(pygame.sprite.Sprite):
+		def __init__(self, location)
+			"""
+			location :: (int, int) - location to be placed in px
+			"""
+			pygame.sprite.Sprite.__init__(self)
+			self.image = pygame.image.load("enemy.png")
+			self.rect = self.image.get_rect()
+			
+			self.position = location
+			self.rect.center = location
+			
+		def update(self)
+			self.position = (self.position[0]+1, self.position[1])
+			self.rect.center = self.position
+			
