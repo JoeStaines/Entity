4d5098ff4a69a5a38c856ac09ee9adfa4f100db7
Nathan D Karimian <ndk01u@avon.cs.ad.nottingham.ac.uk>
2012-04-23 18:16:34 +0100

    added

--- game_06.py
+++ game_06.py
@@ -1,44 +1,60 @@
 import pygame, sys
 from pygame.locals import *
 
-"""
-def main():
-    pygame.init()
-    window = pygame.display.set_mode((400,400),0,32)
-    screen = pygame.display.get_surface()
-    color = pygame.Color(20,40,80)
-    mx,my = 0,0
-    obj = pygame.image.load('sprite.png').convert_alpha()
-    screen.fill(color)
-    pygame.mouse.set_visible(0)
-    
-    while 1:
-        objR = obj.get_rect()
-        objR.center = (mx,my)
+class Entity():
+        def __init__(self):
+                self.mX,self.mY = 0,0
+                self.color = pygame.Color(200,240,60)
+                self.setDisplay()
+                self.addGroup()
+                self.addSprites()
+                
+        
+        def setDisplay(self):
+                self.window = pygame.display.set_mode((400,400),0,32)
+                self.playArea = pygame.display.get_surface()
+                pygame.display.set_caption("Entity")
+                pygame.mouse.set_visible(0)
 
-        screen.fill(color)
-        screen.blit(obj,objR)
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == MOUSEMOTION:
-                mx,my = event.pos
-        pygame.display.update()
-    
-"""    
+        def addSprites(self):
+                self.player = Player((self.mX,self.mY))
+                self.allSprites.add(self.player)
 
+        def addGroup(self):
+                self.allSprites = pygame.sprite.Group()
 
-def main():
-	#some shit
-	pygame.init()
-	
+        def main(self):
+                while 1:
+                        self.playArea.fill(self.color)
+                        self.player.move(self.mX,self.mY)
+                        self.allSprites.update()
+                        self.allSprites.draw(self.playArea)
 
-class player(pygame.sprite.Sprite):
-	def __init__(self,setIniLoc):
-		
+                        for event in pygame.event.get():
+                                if event.type == QUIT:
+                                        pygame.quit()
+                                        sys.exit()
+                                elif event.type == MOUSEMOTION:
+                                        self.mX,self.mY = event.pos
+                                        
+                        pygame.display.update()
 
+class Player(pygame.sprite.Sprite):
+        def __init__(self,location):
+                pygame.sprite.Sprite.__init__(self)
+                self.image = pygame.image.load("sprite.png")
+                self.rect = self.image.get_rect()
+                self.rect.center = location
+                self.position = location
+                #set sprite to mouse position
 
+        def update(self):
+                self.rect.center = self.position
 
-main()
+                
+        def move(self,mX,mY):
+                self.position = (mX,mY)                
+                
+if __name__ == "__main__":
+        Entity().main()
         
